aa8440e147299368f2060030f437780c64112d77
justingreerbbi <justingreerbbi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-13 02:31:36 +0000

    Tagging version 3.0.4
    git-svn-id: https://plugins.svn.wordpress.org/oauth2-provider/tags/3.0.4@1111651 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/admin-options.php
@@ -0,0 +1,328 @@
+<?php
+class WPOAuth_Admin {
+
+	/**
+	 * WO Options Name
+	 * @var string
+	 */
+	protected $option_name = 'wo_options';
+
+	/**
+	 * [_init description]
+	 * @return [type] [description]
+	 */
+	public static function init() {
+		add_action('admin_init', array(new self, 'admin_init'));
+		add_action('admin_menu', array(new self, 'add_page'));
+	}
+
+	/**
+	 * [admin_init description]
+	 * @return [type] [description]
+	 */
+	public function admin_init() {
+		register_setting('wo_options', $this->option_name, array($this, 'validate'));
+	}
+
+	/**
+	 * [add_page description]
+	 */
+	public function add_page() {
+		add_options_page('OAuth Server Settings', 'OAuth Server', 'manage_options', 'wo_settings', array($this, 'options_do_page'));
+	}
+
+	/**
+	 * loads the plugin styles and scripts into scope
+	 * @return [type] [description]
+	 */
+	public function admin_head() {
+		wp_enqueue_style('wo_admin');
+		wp_enqueue_script('wo_admin');
+		wp_enqueue_script('jquery-ui-tabs');
+	}
+
+	/**
+	 * [options_do_page description]
+	 * @return [type] [description]
+	 */
+	public function options_do_page() {
+		$options = get_option($this->option_name);
+		$this->admin_head();
+		$scopes = apply_filters('WO_Scopes', null);
+		error_reporting(0);
+		add_thickbox();
+		?>
+			<div class="wrap">
+	      <h2>Server Confirguration</h2>
+	      <p>Need some help? <a class="button" href="https://wp-oauth.com/knowledge-base">Check out the Documentation</a></p>
+      	<form method="post" action="options.php">
+					<?php settings_fields('wo_options');?>
+        	<div id="wo_tabs">
+						<ul>
+					  	<li><a href="#general-settings">General Settings</a></li>
+					  	<li><a href="#advanced-configuration">Advanced Configuration</a></li>
+					  	<li><a href="#clients">Clients</a></li>
+					  	<li><a href="#server-status">Server Status</a></li>
+						</ul>
+
+						<!-- GENERAL SETTINGS -->
+						<div id="general-settings">
+					  	<table class="form-table">
+					  		<tr valign="top">
+	            		<th scope="row">License Key:</th>
+	                <td>
+	                 	<input type="text" name="<?php echo $this->option_name?>[license]" value="<?php echo $options["license"];?>" length="40" style="width:300px;"/>
+	                 	
+	                 	<?php echo license_status(); ?>
+	                  <?php if (!_vl()): ?>
+	                  	<p class="description">Get a license by visiting <a href="https://wp-oauth.com/knowledge-base/" target="_blank">http://wp-oauth.com/pro-license</a>.</p>
+	                	<?php else: ?>
+	                		<p class="description" style="color:green;">Congratulations and Thank You. Your license is <strong>valid</strong>.</p>
+	                	<?php endif; ?>
+	                </td>
+	              </tr>
+	            	<tr valign="top">
+	            		<th scope="row">API Enabled:</th>
+	                	<td>
+	                  	<input type="checkbox" name="<?php echo $this->option_name?>[enabled]" value="1" <?php echo $options["enabled"] == "1" ? "checked='checked'" : "";?> />
+	                  	<p class="description">If the API is not enabled, it will present requests with an "Unavailable" message.</p>
+	                	</td>
+	              	</tr>
+	            </table>
+					  </div>
+
+					  <!-- ADVANCED CONFIGURATION -->
+					  <div id="advanced-configuration">
+					  	<h2>Advanced Configuration</h2>
+					  	
+	            <h3>Grant Types <hr></h3>
+	            <p>Control which Grant Types that the server will accept.</p>
+							<table class="form-table">
+
+	              <tr valign="top">
+	               	<th scope="row">Authorization Code:</th>
+	                  <td>
+	                  	<input type="checkbox" name="<?php echo $this->option_name?>[auth_code_enabled]" value="1" <?php echo $options["auth_code_enabled"] == "1" ? "checked='checked'" : "";?> />
+	                  	<p class="description">HTTP redirects and WP login form when authenticating.</p>
+	              	  </td>
+	              </tr>
+
+	              <tr valign="top">
+	               	<th scope="row">Client Credentials:</th>
+	                  <td>
+	                  	<input type="checkbox" name="<?php echo $this->option_name?>[client_creds_enabled]" value="1" <?php echo $options["client_creds_enabled"] == "1" ? "checked='checked'" : "";?> />
+	                  	<p class="description">Enable "Client Credentials" Grant Type</p>
+	              	  </td>
+	              </tr>
+
+	              <tr valign="top">
+	               	<th scope="row">User Credentials:</th>
+	                  <td>
+	                  	<input type="checkbox" name="<?php echo $this->option_name?>[user_creds_enabled]" value="1" <?php echo $options["user_creds_enabled"] == "1" ? "checked='checked'" : "";?> />
+	                  	<p class="description">Enable "User Credentials" Grant Type</p>
+	              	  </td>
+	              </tr>
+
+	              <tr valign="top">
+	               	<th scope="row">Refresh Tokens:</th>
+	                  <td>
+	                  	<input type="checkbox" name="<?php echo $this->option_name?>[refresh_tokens_enabled]" value="1" <?php echo $options["refresh_tokens_enabled"] == "1" ? "checked='checked'" : "";?> />
+	                  	<p class="description">Enable "Refresh Token" Grant Type</p>
+	              	  </td>
+	              </tr>
+
+	              <tr valign="top">
+	               	<th scope="row">Allow Implicit:</th>
+	                  <td>
+	                  	<input type="checkbox" name="<?php echo $this->option_name?>[implicit_enabled]" value="1" <?php echo $options["implicit_enabled"] == "1" ? "checked='checked'" : "";?> />
+	                  	<p class="description">Enable "Authorization Code (Implicit)" <a href="http://wp-oauth.com/documentation/server-api/which-grant-type-to-use/">What is this?</a></p>
+	              	  </td>
+	              </tr>
+
+	            </table>
+
+	            <h3>Misc Settings <hr></h3>
+							<table class="form-table">
+								<tr valign="top">
+	               	<th scope="row">Key Length</th>
+	                  <td>
+	                  	<input type="number" name="<?php echo $this->option_name?>[client_id_length]" min="10" value="<?php echo $options["client_id_length"];?>" />
+	                  	<p class="description">Length of Client ID and Client Secrets when generated.</p>
+	              	  </td>
+	              </tr>
+								<tr valign="top">
+	               	<th scope="row">Require Exact Redirect URI:</th>
+	                  <td>
+	                  	<input type="checkbox" name="<?php echo $this->option_name?>[require_exact_redirect_uri]" value="1" <?php echo $options["require_exact_redirect_uri"] == "1" ? "checked='checked'" : "";?> />
+	                  	<p class="description">Enable if exact redirect URI is required when authenticating.</p>
+	              	  </td>
+	              </tr>
+
+	              <tr valign="top">
+	               	<th scope="row">Enforce State Parameter:</th>
+	                  <td>
+	                  	<input type="checkbox" name="<?php echo $this->option_name?>[enforce_state]" value="1" <?php echo $options["enforce_state"] == "1" ? "checked='checked'" : "";?>/>
+	                  	<p class="description">Enable if the "state" paramter is required when authenticating. </p>
+	              	  </td>
+	              </tr>
+							</table>
+
+							<h3>Token Lifetimes <?php echo !_vl()? ' <i style="color:red;font-size:14px;">License Required</i>':'';?> <hr></h3>
+							<p>
+								Take control of your token lifetimes easily. By default Access Tokens are valid for 1 hour
+								and Refresh Tokens are vlaid for 24 hours.
+							</p>
+							<?php if(_vl($options['license'])): ?>
+							<table class="form-table">
+								<tr valign="top">
+	               	<th scope="row">Access Token Lifetime</th>
+	                  <td>
+	                  	<input type="number" name="<?php echo $this->option_name?>[access_token_lifetime]" value="<?php echo $options["access_token_lifetime"];?>" placeholder="3600" />
+	                  	<p class="description">How long an access token is valid (seconds) - Leave blank for default (1 hour)</p>
+	              	  </td>
+	              </tr>
+	              <tr valign="top">
+	               	<th scope="row">Refresh Token Lifetime</th>
+	                  <td>
+	                  	<input type="number" name="<?php echo $this->option_name?>[refresh_token_lifetime]" value="<?php echo $options["refresh_token_lifetime"];?>" placeholder="86400"/>
+	                  	<p class="description">How long a refresh token is valid (seconds)- Leave blank for default (24 hours)</p>
+	              	  </td>
+	              </tr>
+							</table>
+							<?php endif; ?>
+
+							<h3>Firewall <?php echo !_vl($options['license'])? ' <i style="color:red;font-size:14px;">License Required</i>':'';?><hr></h3>
+							<p>
+								The firewall is used to secure your OAuth API by allowing you to block all IP's and only allow
+								approved IP's through. The firewall supports whitelisting of IPV4 and IPv6 addresses.
+							</p>
+							<?php if(_vl($options['license'])): ?>
+							<table class="form-table">
+	              <tr valign="top">
+	               	<th scope="row">Block All Incomming Requests but Whitelisted: </th>
+	                  <td>
+	                  	<input type="checkbox" name="<?php echo $this->option_name?>[firewall_block_all_incomming]" value="1" <?php echo $options["firewall_block_all_incomming"] == "1" ? "checked='checked'" : "";?>/>
+	                  	<p class="description">Block all incomming requests that are not whitelisted below. </p>
+	              	  </td>
+	              </tr>
+
+	              <tr valign="top">
+	               	<th scope="row">IP Whitelist: </th>
+	                  <td>
+	                  	<textarea name="<?php echo $this->option_name?>[firewall_ip_whitelist]" style="margin: 0px;width: 340px;height: 140px;resize: none;" placeholder="127.0.0.1, ::1"><?php echo $options["firewall_ip_whitelist"]; ?></textarea>
+	                  	<p class="description">Enter IP addresses seperated by commas. IPV4 and IPV6 are supported.</p>
+	              	  </td>
+	              </tr>
+							</table>
+						<?php endif; ?>
+
+					  </div>
+
+					  <!-- CLIENTS -->
+					  <div id="clients">
+					  	<h2>
+					  		Clients
+					  		<a href="<?php echo WOURI ; ?>/library/content/create-new-client.php?TB_iframe=true&width=600&height=420" class="add-new-h2 thickbox" title="Add New Client">Add New Client</a>
+					  	</h2>
+
+							<?php
+							$wp_list_table = new WO_Table();
+							$wp_list_table->prepare_items();
+							$wp_list_table->display();
+							?>
+						</div>
+
+						<!-- SERVER STATUS CONTENT -->
+					  <div id="server-status">
+					  	<h2>Server Status</h2>
+					  	<p>
+					  		The following information is helpful when debugging or reporting an issue. Please note that the
+					  		informaiton provided here is a reference only.
+					  	</p>
+					  	<table>
+					  		<tr>
+					  			<th style="text-align:right;">Plugin Build: </th>
+					  			<td>
+										<?php echo strpos(_WO()->version, '-') ? _WO()->version . " <span style='color:orange;'><small>You are using a development version of the plugin.</small></span>" : _WO()->version;?>
+					  			</td>
+					  		</tr>
+
+					  		<tr>
+					  			<th style="text-align:right;">PHP Version (<?php echo PHP_VERSION;?>): </th>
+					  			<td>
+										<?php echo version_compare(PHP_VERSION, '5.3.9') >= 0 ? " <span style='color:green;'>OK</span>" : " <span style='color:red;'>Failed</span> - <small>Please upgrade PHP to 5.4 or greater.</small>";?>
+					  			</td>
+					  		</tr>
+
+					  		<tr>
+					  			<th style="text-align:right;">Running CGI: </th>
+					  			<td>
+					  				<?php echo substr(php_sapi_name(), 0, 3) != 'cgi' ? " <span style='color:green;'>OK</span>" : " <span style='color:orange;'>Notice</span> - <small>Header 'Authorization Basic' may not work as expected.</small>";?>
+					  			</td>
+					  		</tr>
+
+					  		<tr>
+					  			<th style="text-align:right;">License: </th>
+					  			<td>
+										<?php echo !_vl() ? " <span style='color:orange;'>Standard" : "<span style='color:green;'>Licensed</span>"?>
+									</td>
+					  		</tr>
+					  	</table>
+					  </div>
+
+					</div>
+
+          <p class="submit">
+          	<input type="submit" class="button-primary" value="<?php _e('Save Changes')?>" />
+          </p>
+        </form>
+	    </div>
+<?php
+}
+
+	/**
+	 * WO options validation
+	 * @param  [type] $input [description]
+	 * @return [type]        [description]
+	 */
+	public function validate($input) {
+
+		// Check box values
+		$input["enabled"] = isset($input["enabled"]) ? $input["enabled"] : 0;
+		$input["auth_code_enabled"] = isset($input["auth_code_enabled"]) ? $input["auth_code_enabled"] : 0;
+		$input["client_creds_enabled"] = isset($input["client_creds_enabled"]) ? $input["client_creds_enabled"] : 0;
+		$input["user_creds_enabled"] = isset($input["user_creds_enabled"]) ? $input["user_creds_enabled"] : 0;
+		$input["refresh_tokens_enabled"] = isset($input["refresh_tokens_enabled"]) ? $input["refresh_tokens_enabled"] : 0;
+		$input["implicit_enabled"] = isset($input["implicit_enabled"]) ? $input["implicit_enabled"] : 0;
+
+		$input["require_exact_redirect_uri"] = isset($input["require_exact_redirect_uri"]) ? $input["require_exact_redirect_uri"] : 0;
+		$input["enforce_state"] = isset($input["enforce_state"]) ? $input["enforce_state"] : 0;
+
+		// Only run with valid license
+		$input["blacklist_ip_range_enabled"] = isset($input["blacklist_ip_range_enabled"]) ? $input["blacklist_ip_range_enabled"] : 0;
+		$input["block_all_incomming"] = isset($input["block_all_incomming"]) ? $input["block_all_incomming"] : 0;
+
+		$current_options = get_option($this->option_name);
+		if(!empty($input['license']) && $current_options['license'] != $input['license']){
+			$api_params = array( 
+				'edd_action'=> 'activate_license', 
+				'license' 	=> $input['license'], 
+				'item_name' => urlencode('WP OAuth License'),
+				'url'       => home_url()
+			);
+			$response = wp_remote_get( add_query_arg( $api_params, 'http://wp-oauth.com' ) );
+			if ( !is_wp_error( $response ) ){
+				$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+				$input['license_status'] = $license_data->license;
+			}
+		}elseif($input['license'] == ''){
+				$input['license_status'] = '';
+		}else {
+				$input['license_status'] = $current_options['license_status'];
+		}
+
+
+		return $input;
+	}
+}
+WPOAuth_Admin::init();
--- readme.txt
+++ readme.txt
@@ -4,13 +4,134 @@ Contributors: justingreerbbi
 Donate link: http://justin-greer.com/
 Tags: oauth2, OAuth provider, Provider, OAuth, OAuth client, Single Sign On, SSO
 Requires at least: 3.9
-Tested up to: 4.1
-Stable tag: 1.0.0
+Tested up to: 4.1.1
+Stable tag: 3.0.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Use WordPress to Power Your OAuth Server and become a OAuth Provider
+Use WordPress to Power Your OAuth Server
 
 == Description ==
 
-Support for WP OAuth as been discontinued on WP.ORG and has been moved to WP OAuth. Please visit http://wp-oauth.com for details, support and the plugin itself.
+WordPress OAuth Server allows your WordPress site to become an authentication endpoint as well has allowing to you tie in 3rd party apps and software that requires login.
+
+= Supported Grant Types =
+
+* Authentication Code/Implicit
+* User Credentials
+* Client Credentials
+* Refresh Token
+
+= How to Use =
+
+Once you have WP Oauth Server installed and configured you can begin to setup your clients.
+
+*Authentication Code (Most Common, used for Single Sign On)*
+
+= Upgrading to Pro =  
+Upgrading to the PRO version removes all limitations from the plugin. You can lean more by visiting http://wp-oauth.com
+
+= Minimum Requirements =
+* PHP 5.3.9 or greater *(5.4+ recommended)*
+
+= Other Information =
+
+WordPress OAuth Server does not currently support `Jwt Bearer` or `Crypto Tokens`.
+
+*NOTE: As of 3.0.0, there are no backward compatiability for any version older than 3.0.0. We are working on a migration tool, but as of now any upgrade from 2.x to 3.x will need to be done manaully.*
+
+== Installation ==
+
+1. Upload `oauth-provider` to the `/wp-content/plugins/` directory or use the built in plugin install by WordPress
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Click 'Settings' and then 'permalinks'. Then simply click 'Save Changes' to flush the rewrite rules so that OAuth2 Provider
+1. Your Ready to Rock
+
+== Frequently Asked Questions ==
+
+= How do I add a APP/Client? =
+
+Click on `Settings->OAuth Server`. Click on the `Clients` tab and then `Add New Client`. Enter the client information and your are done.
+
+= Does WordPress OAuth Server Support SSO (Single Sign On) =
+
+Yes, WordPress OAuth Server does support Single Sign On.
+
+= Is there support for this plugin? Can you help me? =
+
+You can visit our <a href="http://wp-oauth.com/forums/" title="WordPress OAuth Server">support forum</a> for support.
+
+= Can you set this up for me on my current website? =
+
+* DRINKS COFFEE * Can I? "YES". You are more than welcome to contact us with if you should ever need assistance.
+
+= How do I use WordPress OAuth Server? =
+
+You can visit <a href="http://wp-oauth.com">http://wp-oauth.com</a>. You will find in-depth documentation as well as examples of how to get started.
+
+== Upgrade Notice ==
+
+Version 2.0.0 and lower are not compatiable with version 3.0.0. If you have built your service using version 2.0.0 or lower, visit <a href="http://wp-oauth.com">http://wp-oauth.com</a> support forums.
+
+For any upgrade, PLEASE PLEASE PLEASE make a full backup of your data. 
+
+== Screenshots ==
+
+1. Adding a Client
+
+== Changelog ==
+
+= 1.0.0 =
+* INITIAL BUILD
+
+= 1.0.1 =
+* Re-worked Readme.txt
+* Fixed absolute paths causing 404 Error when WordPress is running under a sub directory (Using admin_url() currently)
+
+= 1.0.2 = 
+* Fixed Broken login redirect
+
+= 1.0.3 =
+* Fixed Admin URL links for plugin dashboard
+
+= 2.0.0 =
+* Rebuild init plugin code structure for more flexibility and scalability.
+* Added prefix to all DB connections
+* Changed install query to use the InnoDB engine for better support and performance.
+* Fixed improper loading of plugin stylesheet.
+* Removed garbage data when plugin is activated. It was not being used and cluttering the codebase as well as the database.
+* Move action template_redirect to rewrites file
+* Added login form support for installs that are installed in sub directory
+* Added missing in documentation for when calling requesting_token
+* Suppressed some errors that was preventing a proper JSON return when `WP_DEBUG` was enabled.
+* Added a client sample script to help learn the basics of connecting to the provider plugin.
+* Add legacy installer that will hopefully keep old data in tacked while updating to the new structure with no data loss.
+* Removed plugin logging as it was not really needed and caused more issues that it was worth.
+
+= 3.0.0 =
+* Updated and rebuilt structure.
+* Visit <a href="http://wp-oauth.com">http://wp-oauth.com</a> for documentation and more information.
+
+= 3.0.1 =
+* Updated cover image.
+* Fixed documentation links.
+* Added "Server Status" tab
+* Cleaned up "Advanced Configuration" contents.
+
+= 3.0.2 =
+* Readded Authorization Code Enable Option
+* API unavaliable error now uses OAuth Response object
+* API now reports when access token is not provided during resource calls
+
+= 3.0.3 =
+* Modified how clients are added and edited
+* Add Pro Features
+* Added additional information to "Server Status" Tab
+* Minor Clean Up
+
+= 3.0.4 = 
+* Updated Readme.txt content
+* Add more descriptive text during PHP version check
+* Fixed license links
+* Added Access Token and Refresh Token lifetime settings
+* Added upgrade method to ensure proper installing of new features
